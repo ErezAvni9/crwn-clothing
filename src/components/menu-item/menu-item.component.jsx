cb4997370b58473fb607e85463aa986c29b3a391
atb1234 <41204391+atb1234@users.noreply.github.com>
2022-02-01 02:15:34 +0200

    update our menue-items to navigate useNavigate HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import './menu-item.styles..scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div 
-    className={`${size} menu-item`}
-    >
-    <div className='background-image'  
-    style={{
-        backgroundImage: `url(${imageUrl})`
-      }} />
-    <div className='content'>
-       <h1 className='title'>{title.toUpperCase()}</h1>
-       <span className='subtitle'>SHOP NOW</span>
-    </div>
-  </div>
-)
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+  const navigate = useNavigate();
 
-export default MenuItem
+  return (
+    <div className={`${size} menu-item`} onClick={() => navigate(`hats`)}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 
-
-ReactDOM.render(<App />,document.getElementById('root'))
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootElement,
+    )
  
 
 
